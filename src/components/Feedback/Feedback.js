3c1d5a644a0f0fb9fa7c437a986b1cbfe650198d
Volodymyr Bortokhov <bartzh1991@gmail.com>
2022-10-01 15:11:43 +0300

    edit

--- src/components/Feedback/Feedback.js
+++ src/components/Feedback/Feedback.js
@@ -23,8 +23,13 @@ export class Feedback extends Component {
     const { Good, Neutral, Bad } = this.state;
     return Good + Neutral + Bad;
   };
+  positivePercentage = () => {
+    const { Good } = this.state;
+    return Math.round((Good / this.countTotalFeedback()) * 100);
+  };
 
   render() {
+    // this.total = this.countTotalFeedback;
     return (
       <div>
         <Section title="Please leave feedback">
@@ -36,7 +41,11 @@ export class Feedback extends Component {
         <Section title="Statistics"></Section>
 
         {this.countTotalFeedback() > 0 ? (
-          <Statistics options={this.state} />
+          <Statistics
+            options={this.state}
+            total={this.countTotalFeedback()}
+            positivePercentage={this.positivePercentage()}
+          />
         ) : (
           <Notification message="There is no feedback" />
         )}
--- src/components/Feedback/Statistics/Statistics.js
+++ src/components/Feedback/Statistics/Statistics.js
@@ -1,15 +1,17 @@
 import { ListOptions, OptionsItem } from './Statistics.styled';
 import PropTypes from 'prop-types';
-export const Statistics = ({ options: { Good, Neutral, Bad } }) => {
-  const total = Good + Neutral + Bad;
-  const positivePercentage = Math.round((Good / total) * 100);
+export const Statistics = ({
+  options: { Good, Neutral, Bad },
+  total,
+  positivePercentage,
+}) => {
   return (
     <ListOptions>
       <OptionsItem>Good: {Good}</OptionsItem>
       <OptionsItem>Neutral: {Neutral}</OptionsItem>
       <OptionsItem>Bad: {Bad}</OptionsItem>
-      <OptionsItem>Total {total}</OptionsItem>
-      <OptionsItem>Percent {positivePercentage}%</OptionsItem>
+      <OptionsItem>Total: {total}</OptionsItem>
+      <OptionsItem>Percent: {positivePercentage}%</OptionsItem>
     </ListOptions>
   );
 };
@@ -20,4 +22,6 @@ Statistics.propTypes = {
     Neutral: PropTypes.number.isRequired,
     Bad: PropTypes.number.isRequired,
   }),
+  total: PropTypes.number.isRequired,
+  positivePercentage: PropTypes.number.isRequired,
 };
